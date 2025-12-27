c0e39f5eee21dd34c76579f559ab43693fa6ffab
alexandra-ribeiro <48072692+alexandra-ribeiro@users.noreply.github.com>
2025-12-27 15:08:32 +0000

    Update page.tsx

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link"
 import { formatDate } from "@/lib/utils"
 import type { Metadata } from "next"
 import { notFound } from "next/navigation"
-
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 
 const BASE_URL = "https://www.alexandraribeiro.pt"
 
