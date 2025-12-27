44b8fd42bf1f7c6a0691332c13f3d2eb3edce886
alexandra-ribeiro <48072692+alexandra-ribeiro@users.noreply.github.com>
2025-12-27 19:39:45 +0000

    Update page.tsx

--- app/[lang]/blog/tag/[tag]/page.tsx
+++ app/[lang]/blog/tag/[tag]/page.tsx
@@ -1,5 +1,5 @@
 import Script from "next/script"
-import { getDictionary } from "@/lib/dictionaries"
+import { getDictionary } from "../../../../lib/dictionaries"
 
 export default async function BlogTagPage({
   params,
