21d9d01560e4897fac58bc6bcebc49e70de81906
alexandra-ribeiro <48072692+alexandra-ribeiro@users.noreply.github.com>
2025-12-27 16:07:52 +0000

    Update page.tsx

--- app/[lang]/blog/[slug]/page.tsx
+++ app/[lang]/blog/[slug]/page.tsx
@@ -62,13 +62,7 @@ export async function generateMetadata({
   const description =
     post.fields.seoDescription || post.fields.description || ""
 
-   const tagIds = post.metadata.tags.map(tag => tag.sys.id)
-
-const relatedPosts = await getRelatedPostsByTags(
-  tagIds,
-  params.lang,
-  post.fields.slug
-)
+  
 
   const canonicalUrl = `${BASE_URL}/${lang}/blog/${post.fields.slug}`
   const imageUrl = post.fields.featuredImage
